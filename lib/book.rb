3fbebf2b33613fc0311676cca98d046f2b67756b
root <gloria.kitili@student.moringaschool.com>
2023-02-12 12:38:42 +0300

    basics-putting-the-pieces-together-lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, : page_count, :genre
+
+    def intialize (title = "The Monk Who Sold His Ferrari")
+        @title = title
+    end
+
+    def flip_page 
+     puts "Flipping the page to see the next content!"
+    end
+
+end
+Oranges are not the only fruit = Book.new ("Oranges are not the only fruit ")
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,18 @@
 # Make your shoe class here!
+class Shoe 
+    attr_reader :brand
+    atrr_accessor :brand, :color, :texture
+    
+    def initialize (brand = "Flatshoes")
+        @brand = brand
+
+    end
+
+    def cobble
+        @color = "purple"
+        puts "The shoe color is amazing!"
+    end
+
+end
+
+flatshoes = Shoe.new("flatshoes")
